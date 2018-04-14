64e1595ca95be85e4373da7833f17e8aab8b819d
g-uru <22568014+g-uru@users.noreply.github.com>
2018-04-15 01:39:14 +0200

    optimize for non-interactive mode

--- getlms.sh
+++ getlms.sh
@@ -13,6 +13,7 @@ cd /storage/.kodi/docker                                                        
 wget https://github.com/g-uru/docker-logitechmediaserver-rpi2/archive/master.zip          && \
 unzip master.zip 																		  && \
 cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/getlms.sh .            && \
+chmod +x getlms.sh															              && \
 cd docker-logitechmediaserver-rpi2-master                                                 && \
 docker build -t logitechmediaserver-rpi2 .                                                && \
 cd /storage/.kodi/docker                                                                  && \
@@ -25,19 +26,27 @@ echo restarting lms.service
 systemctl start lms
 echo done ...
 
+# for interactive mode
 #read -t 10 -n 1 -p "Show journalctl -u lms? [Y/n] " reply;
 #if [ "$reply" != "" ]; then journalctl -u lms; fi
 #if [ "$reply" != "n" ]; then echo
-#    fi
-	
+#    fi	
 #read -t 10 -n 1 -p "Show more/again? [Y/n] " reply;
 #if [ "$reply" != "" ]; then journalctl -u lms; fi
 #if [ "$reply" != "n" ]; then echo
 #    fi
+#journalctl -u lms;
+#echo "Waiting 10 seconds for LMS to finish then journalctl -u lms again..."
+#sleep 10
+#journalctl -u lms;
+#read -p "Press any key to roboot"
+#reboot now
 
 journalctl -u lms;
-read -t 10 -n 1 -p "Waiting 10 seconds then journalctl -u lms again..."
-journalctl -u lms;
-read -t 10 -n 1 -p "Waiting 20 Seconds for review..."
+echo Waiting 10 Seconds for LMS to finish ...
+sleep 10
+echo Waiting 20 seconds for review ...
+sleep 20
 
+echo Rebooting now ...
 reboot now
