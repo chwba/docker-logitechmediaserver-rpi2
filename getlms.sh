990af012a1801be3f2fdaf2e135d1ef92fbe8c20
g-uru <22568014+g-uru@users.noreply.github.com>
2018-04-15 20:47:07 +0200

    change getlms.sh

--- getlms.sh
+++ getlms.sh
@@ -1,24 +1,22 @@
-#!/usr/bin/sh
-set -x 
-echo stopping lms.service 
-systemctl stop lms 
-sleep 5 
+#!/bin/sh
+set -x
+echo stopping lms.service
+systemctl stop lms
+sleep 2
+/storage/.kodi/addons/service.system.docker/bin/docker stop $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq)
+/storage/.kodi/addons/service.system.docker/bin/docker rm $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq)
+/storage/.kodi/addons/service.system.docker/bin/docker rmi $(/storage/.kodi/addons/service.system.docker/bin/docker images -aq)
+/storage/.kodi/addons/service.system.docker/bin/docker system prune -af
 
-/storage/.kodi/addons/service.system.docker/bin/docker stop $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq) 
-/storage/.kodi/addons/service.system.docker/bin/docker rm $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq) 
-/storage/.kodi/addons/service.system.docker/bin/docker rmi $(/storage/.kodi/addons/service.system.docker/bin/docker images -aq) 
-/storage/.kodi/addons/service.system.docker/bin/docker system prune -af 
+cd /storage/.kodi/docker
+wget https://github.com/g-uru/docker-logitechmediaserver-rpi2/archive/master.zip
+unzip master.zip
+cd docker-logitechmediaserver-rpi2-master
+echo rebuilding Docker...
+/storage/.kodi/addons/service.system.docker/bin/docker build -t logitechmediaserver-rpi2 .										
 
-cd /storage/.kodi/docker                                                               
-wget https://github.com/g-uru/docker-logitechmediaserver-rpi2/archive/master.zip         
-unzip master.zip 																		
-cd docker-logitechmediaserver-rpi2-master                                                
-docker build -t logitechmediaserver-rpi2 .                                                
-
-sleep 5 
-echo restarting lms.service 
-systemctl start lms 
-echo done ... 
+echo restarting lms.service
+systemctl start lms
 
 # for interactive mode
 #read -t 10 -n 1 -p "Show journalctl -u lms? [Y/n] " reply;
@@ -33,20 +31,20 @@ echo done ...
 #echo "Waiting 10 seconds for LMS to finish then journalctl -u lms again..."
 #sleep 10
 #journalctl -u lms;
-#read -p "Press any key to roboot"
+#read -p "Press any key to reboot"
 #reboot now
 
-cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/getlms.sh .        
-chmod +x getlms.sh															         
-rm -f master.zip      																	
-cd  /storage/.kodi/docker                                                              
-rm -f -r docker-logitechmediaserver-rpi2-master                                  
-ls -la 
-journalctl -u lms
-echo Waiting 10 Seconds for LMS to finish ... 
-sleep 10 
-echo Waiting 20 seconds for review ... 
-sleep 20 
+echo Delete tmpfiles..update getlms.sh...
+cd /storage/.kodi/docker
+cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/getlms.sh /storage/.kodi/docker
+chmod +x /storage/.kodi/docker/getlms.sh
+rm -f master.zip
+rm -f -r docker-logitechmediaserver-rpi2-master
+ls -la
 
-echo Rebooting now ... 
+echo Waiting 20 Seconds for LMS to finish ...
+sleep 20
+journalctl -u lms
+echo Rebooting in 5 seconds  ...
+sleep 5
 reboot now
