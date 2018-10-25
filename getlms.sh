c0e1de418a95481565249b14c60abe7089ee5733
chwba <22568014+chwba@users.noreply.github.com>
2018-10-25 14:31:12 +0200

    minor change in getlms.sh

--- getlms.sh
+++ getlms.sh
@@ -3,6 +3,8 @@ set -x
 echo stopping lms.service
 systemctl stop lms
 systemctl stop lmslog.timer
+systemctl disable lms
+systemctl disable lmslog.timer
 sleep 2
 /storage/.kodi/addons/service.system.docker/bin/docker stop $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq)
 /storage/.kodi/addons/service.system.docker/bin/docker rm $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq)
@@ -11,30 +13,21 @@ sleep 2
 
 cd /storage/.kodi/docker
 wget https://github.com/chwba/docker-logitechmediaserver-rpi2/archive/master.zip
+
 unzip master.zip
 cd docker-logitechmediaserver-rpi2-master
+cp -f *.service /storage/.kodi/addons/service.system.docker/examples
+cp -f *.timer /storage/.kodi/addons/service.system.docker/examples
 echo rebuilding Docker...
 /storage/.kodi/addons/service.system.docker/bin/docker build -t logitechmediaserver-rpi2 .										
 
 echo restarting services ...
+systemctl enable /storage/.kodi/addons/service.system.docker/examples/lms.service
+systemctl enable /storage/.kodi/addons/service.system.docker/examples/lmslog.service
+systemctl enable /storage/.kodi/addons/service.system.docker/examples/lmslog.timer
 systemctl start lms
+systemctl start lmslog
 systemctl start lmslog.timer
-
-# for interactive mode
-#read -t 10 -n 1 -p "Show journalctl -u lms? [Y/n] " reply;
-#if [ "$reply" != "" ]; then journalctl -u lms; fi
-#if [ "$reply" != "n" ]; then echo
-#    fi	
-#read -t 10 -n 1 -p "Show more/again? [Y/n] " reply;
-#if [ "$reply" != "" ]; then journalctl -u lms; fi
-#if [ "$reply" != "n" ]; then echo
-#    fi
-#journalctl -u lms;
-#echo "Waiting 10 seconds for LMS to finish then journalctl -u lms again..."
-#sleep 10
-#journalctl -u lms;
-#read -p "Press any key to reboot"
-#reboot now
 
 echo Delete tmpfiles..update getlms.sh...
 cd /storage/.kodi/docker
