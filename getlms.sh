6f5caec1575eb51d723cf65806ef71e67d5e4d36
chwba <22568014+chwba@users.noreply.github.com>
2018-11-24 15:21:06 +0100

    ....

--- getlms.sh
+++ getlms.sh
@@ -2,54 +2,38 @@
 set -x
 echo "######################################################################################################################################################" >> /storage/Tomato/logs/getlms.log
 echo stopping lms.service
-systemctl stop lmslog.timer && \
-systemctl stop lms && \
+systemctl stop lmslog.timer
+systemctl stop lms
 sleep 3
-#systemctl disable lms
-#systemctl disable lmslog.service
-#systemctl disable lmslog.timer
-/storage/.kodi/addons/service.system.docker/bin/docker stop $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq) && \
-/storage/.kodi/addons/service.system.docker/bin/docker rm $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq) && \
-/storage/.kodi/addons/service.system.docker/bin/docker rmi $(/storage/.kodi/addons/service.system.docker/bin/docker images -aq) && \
-/storage/.kodi/addons/service.system.docker/bin/docker system prune -af && \
+systemctl disable lms
+systemctl disable lmslog.service
+systemctl disable lmslog.timer
+/storage/.kodi/addons/service.system.docker/bin/docker stop $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq)
+/storage/.kodi/addons/service.system.docker/bin/docker rm $(/storage/.kodi/addons/service.system.docker/bin/docker ps -aq)
+/storage/.kodi/addons/service.system.docker/bin/docker rmi $(/storage/.kodi/addons/service.system.docker/bin/docker images -aq)
+/storage/.kodi/addons/service.system.docker/bin/docker system prune -af
 
-#cd /storage/.kodi/docker
-#wget https://github.com/chwba/docker-logitechmediaserver-rpi2/archive/master.zip && unzip master.zip && \
-#cd docker-logitechmediaserver-rpi2-master
-
-# overwrite service files
-#cp -f *.service /storage/.kodi/addons/service.system.docker/examples
-#cp -f *.service /storage/.config/system.d/
-#cp -f *.timer /storage/.kodi/addons/service.system.docker/examples
-#cp -f *.timer /storage/.config/system.d/
+cd /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master
 
 echo rebuilding Docker...
-/storage/.kodi/addons/service.system.docker/bin/docker build -t logitechmediaserver-rpi2 . && \
+/storage/.kodi/addons/service.system.docker/bin/docker build -t logitechmediaserver-rpi2 .
 
 echo restarting services ...
-#systemctl enable lms.service
-#systemctl enable lmslog.service
-#systemctl enable lmslog.timer
-systemctl start lms && \
-systemctl start lmslog.timer && \
+systemctl enable lms.service
+systemctl enable lmslog.service
+systemctl enable lmslog.timer
+systemctl start lms
+systemctl start lmslog.timer
 
 
-echo "Delete tmpfiles..overwrite .sh-files"
+echo "Delete tmpfiles..overwrite .init.sh..."
+cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master /storage/.kodi/docker/
 cd /storage/.kodi/docker
-#cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/getlms.sh /storage/.kodi/docker && \
-#cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/lmsup.sh /storage/.kodi/docker && \
-# chmod +x /storage/.kodi/docker/getlms.sh
-# chmod +x /storage/.kodi/docker/lmsup.sh
-rm -f master.zip
-rm -f -r docker-logitechmediaserver-rpi2-master
+rm -f /storage/.kodi/docker/master.zip
+rm -f -r /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master
+rm -f /storage/.kodi/docker/getlms.sh
 
-
-#echo Waiting 20 Seconds for LMS to finish ...
-#sleep 20
-#journalctl -u lms
-#echo Rebooting in 5 seconds  ...
-#sleep 5
-echo "rebooting..."
+echo "Rebooting..."
 reboot now
 echo "######################################################################################################################################################" >> /storage/Tomato/logs/getlms.log
 
---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo get master from repo...
+cd /storage/.kodi/docker
+wget https://github.com/chwba/docker-logitechmediaserver-rpi2/archive/master.zip && unzip master.zip
+cd /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master
+
+# overwrite service files
+echo copy service files frop repo...
+cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/*.service /storage/.kodi/addons/service.system.docker/examples
+cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/*.service /storage/.config/system.d/
+cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/*.timer /storage/.kodi/addons/service.system.docker/examples
+cp -f /storage/.kodi/docker/docker-logitechmediaserver-rpi2-master/*.timer /storage/.config/system.d/
+systemctl daemon-reload
+
+echo copy sh files from repo...
+cp -f getlms.sh /storage/.kodi/docker
+cp -f lmsup.sh /storage/.kodi/docker
+chmod +x /storage/.kodi/docker/getlms.sh
+chmod +x /storage/.kodi/docker/lmsup.sh
+
+echo start getlms.sh...
+exec /storage/.kodi/docker/getlms.sh
